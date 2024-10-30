0df11769ccf84c5053ba111f7ba987ed5680fd95
daniel.tollstorp@knowit.se <daniel.tollstorp@knowit.se>
2024-10-30 07:08:25 +0100

    test commit

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,7 +3,6 @@ package org.example;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.example.Calculator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
