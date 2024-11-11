416c8b5b2eea5f918b045f9ebfa968e9a5bbb6b4
daniel.tollstorp@knowit.se <daniel.tollstorp@knowit.se>
2024-11-11 18:39:18 +0100

    Bypassing rule and push

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -26,6 +26,7 @@ public class CalculatorTest {
         //EDIT 11/11, this test passes now, should be ok for commit and push
         //small change
         //change
+        //change
     }
 
     @Test
